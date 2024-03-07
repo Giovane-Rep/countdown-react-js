1b23842bc2b712be564c93d7e00d5825c24e050f
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2024-03-07 13:45:11 -0300

    Basic components and css

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import "./Counter.css";
+
+const Counter = ({ title, number }) => {
+  return (
+    <div className="counter">
+      <p className="counter-number">{number}</p>
+      <h3 className="counter-text">{title}</h3>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/components/Title.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import "./Title.css";
+
+export const Title = ({ title }) => {
+  return <h1 className="title">{title}</h1>;
+};
