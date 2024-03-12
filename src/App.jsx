0347e64065cff2cf1c4ee577d739c4a200f603d6
GiovaneDiasRocha <66972321+GiovaneDiasRocha@users.noreply.github.com>
2024-03-11 21:42:25 -0300

    Updating the countdown

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,13 @@ import "./App.css";
 
 import FireworkImage from "./assets/fireworks.jpg";
 
-import { Title } from "./components/Title";
-
-import Counter from "./components/Counter";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${FireworkImage})` }}>
       <div className="container">
-        <Title title="Contagem regressiva para 2024" />
-        <div className="countdown-container">
-          <Counter title="Dias" number="2" />
-          <Counter title="Horas" number="1" />
-          <Counter title="Minutos" number="10" />
-          <Counter title="Segundos" number="18" />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
